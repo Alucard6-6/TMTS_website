cc46fcb10c75f75032e7e0fe0681971e987da295
Alucard6-6 <c.liu.alucard@gmail.com>
2026-07-17 16:11:56 +0200

    Add files via upload

--- TMTS_for_website.py
+++ TMTS_for_website.py
@@ -94,7 +94,7 @@ if run_match and reaction_ok:
     arr_dic=addnewkinetic(detailed_dic_reaction)
     if len(arr_dic)>0:
         model=list(arr_dic["Target"][3][0].keys())[0]
-        A,n,Ea=arr_dic["Target"][3][0][model][0],arr_dic["Target"][3][0][model][1],arr_dic["Target"][3][0][model][2]
+        A,n,Ea,Lcmodfin=arr_dic["Target"][3][0][model][0],arr_dic["Target"][3][0][model][1],arr_dic["Target"][3][0][model][2]
         #st.write('Read the reaction in the format of a dictionary:',detailed_dic_reaction)
         st.write('The Arrhenius parameters for this reaction fitted between 500 and 2000K are (in cal, mol, s units):')
         res1,res2=st.columns(2)
@@ -102,13 +102,13 @@ if run_match and reaction_ok:
             st.write('Pre-exponential parameter:')
             st.write('n parameter:')    
             st.write('Activation energy:')
-            #st.write('External symmetry:')
+            st.write('Lc model final:')
             # st.write('External symmetry (CCCCC):')
         with res2:
             st.write(f'{A}')
             st.write(f'{n}')
             st.write(f'{Ea}')
-            #st.write(f'{extsym}')
+            st.write(f'{Lcmodfin}')
             # st.write(f'{extsymtest}')
     else:
         st.write("We are sorry, but this reaction is not covered by our database")
